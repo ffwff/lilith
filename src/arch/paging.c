1cd8ad206a57afed176fb029763b1ec7b0eead2f
ffwff <mogamiriver@tuta.io>
2019-07-04 15:05:59 +0700

    move paging setup to crystal

--- src/arch/paging.c
+++ src/arch/paging.c
@@ -2,26 +2,6 @@
 #include "arch/pmalloc.h"
 #include "stdint.h"
 
-// NOTE: we only do identity paging
-
-// paging control
-static void enable_paging() {
-    uint32_t cr0;
-    asm volatile("mov %%cr0, %0"
-                 : "=r"(cr0));
-    cr0 |= 0x80000000;  // Enable paging!
-    asm volatile("mov %0, %%cr0" ::"r"(cr0));
-}
-
-static void disable_paging() {
-    uint32_t cr0;
-    asm volatile("mov %%cr0, %0"
-                 : "=r"(cr0));
-    cr0 &= ~(1 << 31);  // clear PG bit
-    asm volatile("mov %0, %%cr0" ::"r"(cr0));
-}
-
-//
 struct page {
     uint32_t present : 1;
     uint32_t rw : 1;
@@ -44,55 +24,49 @@ static struct page page_create(int rw, int user, uint32_t phys_addr) {
     return page;
 }
 
-//
 struct page_table {
     struct page pages[1024];  // 1024*4kb = 4MB
 };
 
-//
 struct page_directory {
     struct page_table *tables[1024];
     uint32_t tables_physical[1024];
 };
 
-static void alloc_page(struct page_directory *dir, int rw, int user, uint32_t address) {
+struct page_directory *kernel_page_dir = 0;
+
+// impl
+void kalloc_page(int rw, int user, uint32_t address) {
     uint32_t phys = address;
     address /= 0x1000;
     uint32_t table_idx = address / 1024;
-    if (dir->tables[table_idx] == 0) {
+    if (kernel_page_dir->tables[table_idx] == 0) {
         uint32_t ptr;
-        dir->tables[table_idx] = pmalloc_a(sizeof(struct page_table), &ptr);
-        dir->tables_physical[table_idx] = (uint32_t)ptr | 0x7;
+        kernel_page_dir->tables[table_idx] = pmalloc_a(sizeof(struct page_table), &ptr);
+        kernel_page_dir->tables_physical[table_idx] = (uint32_t)ptr | 0x7;
     }
-    dir->tables[table_idx]->pages[address % 1024] = page_create(rw, user, (phys & 0xFFFFF000) >> 8);
+    kernel_page_dir->tables[table_idx]->pages[address % 1024] = page_create(rw, user, (phys & 0xFFFFF000) >> 8);
 }
 
-#define ALIGNED(x) ((((uint32_t)x)&0xFFFFF000)+0x1000)
+void kinit_paging() {
+    kernel_page_dir = pmalloc_a(sizeof(struct page_directory), 0);
+    memset(kernel_page_dir, 0, sizeof(struct page_directory));
+}
 
-struct page_directory *kernel_dir;
-void kinit_paging(void *text_start, void *text_end,
-                void *data_start, void *data_end,
-                void *stack_start, void *stack_end) {
-    kernel_dir = pmalloc_a(sizeof(struct page_directory), 0);
-    memset(kernel_dir, 0, sizeof(struct page_directory));
-    alloc_page(kernel_dir, 0, 0, 0xb8000); // VGA
-    // text segment
-    for (uint32_t i = (uint32_t)text_start; i <= ALIGNED(text_end); i += 0x1000) {
-        alloc_page(kernel_dir, 0, 0, i);
-    }
-    // data segment
-    for (uint32_t i = (uint32_t)data_start; i <= ALIGNED(data_end); i += 0x1000) {
-        alloc_page(kernel_dir, 1, 0, i);
-    }
-    // stack segment
-    for (uint32_t i = (uint32_t)stack_start; i <= ALIGNED(stack_end); i += 0x1000) {
-        alloc_page(kernel_dir, 1, 0, i);
-    }
-    // heap
-    for (uint32_t i = pmalloc_start; i < pmalloc_addr; i += 0x1000) {
-        alloc_page(kernel_dir, 1, 1, i);
-    }
-    // switch page directory
-    asm volatile("mov %0, %%cr3" ::"r"((uint32_t)kernel_dir->tables_physical));
-    enable_paging();
+// paging control
+void kenable_paging() {
+    asm volatile("mov %0, %%cr3" ::"r"((uint32_t)kernel_page_dir->tables_physical));
+    uint32_t cr0;
+    asm volatile("mov %%cr0, %0"
+                 : "=r"(cr0));
+    cr0 |= 0x80000000;  // Enable paging!
+    asm volatile("mov %0, %%cr0" ::"r"(cr0));
+}
+
+void kdisable_paging() {
+    uint32_t cr0;
+    asm volatile("mov %%cr0, %0"
+                 : "=r"(cr0));
+    cr0 &= ~(1 << 31);  // clear PG bit
+    asm volatile("mov %0, %%cr0" ::"r"(cr0));
 }
--- src/arch/paging.cr
+++ src/arch/paging.cr
@@ -1,9 +1,11 @@
 private lib Kernel
-    fun kinit_paging(
-        text_start : Void*, text_end : Void*,
-        data_start : Void*, data_end : Void*,
-        stack_end : Void*, stack_start : Void*
-    )
+    fun kinit_paging()
+    fun kenable_paging()
+    fun kdisable_paging()
+    fun kalloc_page(rw : Int32, user : Int32, address : UInt32)
+    $kernel_page_dir : Void*
+    $pmalloc_start : Void*
+    $pmalloc_addr : Void*
 end
 
 module Paging
@@ -14,9 +16,55 @@ module Paging
         data_start : Void*, data_end : Void*,
         stack_end : Void*, stack_start : Void*
     )
-        Kernel.kinit_paging(text_start, text_end,
-                    data_start, data_end,
-                    stack_start, stack_end)
+        Kernel.kinit_paging()
+        alloc_page 0, 0, 0xb8000
+        # text segment
+        i = text_start.address.to_u32
+        while i <= aligned(text_end.address.to_u32)
+            alloc_page 0, 0, i
+            i += 0x1000
+        end
+        # data segment
+        i = data_start.address.to_u32
+        while i <= aligned(data_end.address.to_u32)
+            alloc_page 1, 0, i
+            i += 0x1000
+        end
+        # stack segment
+        i = stack_start.address.to_u32
+        while i <= aligned(stack_end.address.to_u32)
+            alloc_page 1, 0, i
+            i += 0x1000
+        end
+        # heap
+        i = Kernel.pmalloc_start.address.to_u32
+        while i <= Kernel.pmalloc_addr.address.to_u32
+            alloc_page 1, 0, i
+            i += 0x1000
+        end
+        # -- switch page directory
+        enable
+    end
+
+    private def aligned(x : UInt32) : UInt32
+        (x & 0xFFFFF000) + 0x1000
+    end
+
+    # state
+    @[AlwaysInline]
+    def enable
+        Kernel.kenable_paging
+    end
+
+    @[AlwaysInline]
+    def disable
+        Kernel.kdisable_paging
+    end
+
+    # page alloc
+    @[AlwaysInline]
+    def alloc_page(rw : Int32, user : Int32, address : UInt32)
+        Kernel.kalloc_page rw, user, address
     end
 
 end
