2f72fec69ceef548c316259cca3f0d9c02c334f0
ffwff <mogamiriver@tuta.io>
2019-07-15 22:29:35 +0700

    use cparser as c compiler for userspace

--- userspace/compile.sh
+++ userspace/compile.sh
@@ -2,6 +2,6 @@ if [[ "x$CC" = "x" ]]; then
     CC=clang
 fi
 
-ARCH=i686-elf
-$CC -c -o $1.o $1 -target $ARCH -nostdlib -nostdinc -O2
-ld -m elf_i386 -T link.ld -o $(basename "$1" .c).bin $1.o
+ARCH=i386-elf-lilith
+$CC -o $1.bin $1 -target $ARCH -nostdlib -nostdinc -O2
+# ./toolchain/tools/bin/i386-elf-lilith-ld -o $(basename "$1" .c).bin $1.o
