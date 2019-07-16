a56a5bee22d20bbca6c713e0288e6d4a00bf3aa7
ffwff <mogamiriver@tuta.io>
2019-07-16 17:35:18 +0700

    reorganize userspace directory

--- userspace/compile.sh
+++
@@ -1,7 +0,0 @@
-if [[ "x$CC" = "x" ]]; then
-    CC=clang
-fi
-
-ARCH=i386-elf-lilith
-$CC -o $1.bin $1 -target $ARCH -nostdlib -nostdinc -O2
-# ./toolchain/tools/bin/i386-elf-lilith-ld -o $(basename "$1" .c).bin $1.o
