cac0afa3026729eac36d805c5b9e0102c4e85473
ffwff <mogamiriver@tuta.io>
2019-07-28 21:04:56 +0700

    os: remove test programs, handle release flag in libc compilation

--- userspace/programs/fork.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <syscalls.h>
-
-int main() {
-    printf("fork\n");
-    spawn("/ata0/main.bin");
-}
--- userspace/programs/input.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("type a number:\n");
-    int d;
-    scanf("%d", &d);
-    printf("number + 1 = %d\n", d + 1);
-    while(1) {}
-}
