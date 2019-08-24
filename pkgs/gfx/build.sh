5370e6b9b2cf432a2e2da32c70cdfb67513d253a
ffwff <mogamiriver@tuta.io>
2019-08-24 20:27:59 +0700

    os: add cairo patch, cairo compilation script

--- pkgs/gfx/build.sh
+++ pkgs/gfx/build.sh
@@ -1,12 +1,4 @@
-_download() {
-    if [[ ! -f "$2" ]]; then
-        wget -O"$2" "$1"
-    fi
-}
-
 build() {
-    _download https://raw.githubusercontent.com/nothings/stb/master/stb_image.h "$build_dir/stb_image.h"
-
     for i in $script_dir/*.c; do
         ${opt_arch}-gcc -O2 -o $build_dir/$(basename $i .c) $i -lm
     done
@@ -16,5 +8,4 @@ install() {
     for i in $script_dir/*.c; do
         sudo cp $build_dir/$(basename $i .c) $install_dir/bin
     done
-    sudo cp $build_dir/test.png $install_dir/test.png
 }
