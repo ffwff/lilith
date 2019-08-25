b5046e58b9455fd798a765bb0c50048255ffa23b
ffwff <mogamiriver@tuta.io>
2019-08-25 23:01:16 +0700

    os: make cairo demo work

--- pkgs/gfx/cairodem.c
+++ pkgs/gfx/cairodem.c
@@ -1,7 +1,14 @@
+#include <stdio.h>
 #include <cairo/cairo.h>
 
 int main(int argc, char const **argv)
 {
-    cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, 640, 480);
+    cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, 512, 512);
+    cairo_t *cr = cairo_create(surface);
+    cairo_rectangle(cr, 0.0, 0.0, 512.0, 512.0);
+    cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+    cairo_fill(cr);
+    printf("%x\n", cairo_image_surface_get_data(surface)[0]);
     return 0;
+
 }
