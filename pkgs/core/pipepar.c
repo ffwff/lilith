35f1fae31967f7b305226d844d603bf6f25c0725
ffwff <mogamiriver@tuta.io>
2019-09-02 16:58:36 +0700

    pipepar: send message two times
    - libc: add sleep macro

--- pkgs/core/pipepar.c
+++ pkgs/core/pipepar.c
@@ -8,8 +8,14 @@ int main(int argc, char **argv) {
     int fd = create("/pipes/example");
     printf("sending \"%s\"...\n", str);
     write(fd, str, strlen(str));
+
     printf("spawning child\n");
     char *sargv[2] = { "pipechd", NULL };
     spawnv("pipechd", (char**)sargv);
+
+    sleep(1);
+    printf("sending \"%s\"...\n", str);
+    write(fd, str, strlen(str));
+
     return 0;
 }
