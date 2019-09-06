e6f2cb15322b2a88cc10ddc4f9fcd8c36918e49d
ffwff <mogamiriver@tuta.io>
2019-09-06 21:44:24 +0700

    wm: add configure atom, event buffer

--- pkgs/wm/wm.c
+++ pkgs/wm/wm.c
@@ -50,8 +50,8 @@ static void panic(const char *s) {
 
 #define min(x, y) ((x)<(y)?(x):(y))
 
-#define PACKET_TIMEOUT 1000000
-#define FRAME_TICK  1000000/60
+#define PACKET_TIMEOUT 100000
+#define FRAME_WAIT 10000
 
 /* WM State */
 
@@ -60,12 +60,17 @@ struct wm_window;
 struct wm_window_prog;
 struct wm_window_sprite;
 
+#define WM_EVENT_QUEUE_LEN 128
+
 struct wm_state {
     int needs_redraw;
     int mouse_fd;
     struct wm_window *mouse_win; // weakref
+    struct wm_atom last_mouse_atom;
     struct wm_window *windows;
     int nwindows;
+    struct wm_atom queue[WM_EVENT_QUEUE_LEN];
+    int queue_len;
 };
 
 #define WM_WINDOW_PROG   0
@@ -73,6 +78,7 @@ struct wm_state {
 
 struct wm_window_prog {
     int mfd, sfd;
+    unsigned long event_mask;
 };
 
 struct wm_window_sprite {
@@ -81,6 +87,7 @@ struct wm_window_sprite {
 
 struct wm_window {
     int type;
+    int wid;
     union {
         struct wm_window_prog prog;
         struct wm_window_sprite sprite;
@@ -93,9 +100,9 @@ struct wm_window {
 int win_write_and_wait(struct wm_window_prog *prog,
                        struct wm_atom *write_atom,
                        struct wm_atom *respond_atom) {
+    ftruncate(prog->mfd, 0);
     write(prog->mfd, (char *)write_atom, sizeof(struct wm_atom));
     if (waitfd(prog->sfd, PACKET_TIMEOUT) < 0) {
-        ftruncate(prog->mfd, 0);
         ftruncate(prog->sfd, 0);
         return 0;
     }
@@ -103,11 +110,14 @@ int win_write_and_wait(struct wm_window_prog *prog,
     return read(prog->sfd, (char *)respond_atom, sizeof(struct wm_atom));
 }
 
-/* IMPL */
+/* WINDOW */
 
 struct wm_window *wm_add_window(struct wm_state *state) {
     state->windows = realloc(state->windows, state->nwindows + 1);
-    return &state->windows[state->nwindows++];
+    struct wm_window *win = &state->windows[state->nwindows++];
+    memset(win, 0, sizeof(struct wm_window));
+    win->wid = state->nwindows;
+    return win;
 }
 
 struct wm_window *wm_add_win_prog(struct wm_state *state) {
@@ -135,6 +145,16 @@ struct wm_window *wm_add_sprite(struct wm_state *state, struct wm_window_sprite 
     return win;
 }
 
+void wm_handle_request_atom(struct wm_state *state, struct wm_window *win, struct wm_atom *atom) {
+    switch (atom->type) {
+        case ATOM_CONFIGURE_TYPE: {
+            win->as.prog.event_mask = atom->configure.event_mask;
+        }
+    }
+}
+
+/* IMPL */
+
 int wm_sort_windows_by_z_compar(const void *av, const void *bv) {
     const struct wm_window *a = av, *b = bv;
     if (a->z_index < b->z_index) return -1;
@@ -143,7 +163,26 @@ int wm_sort_windows_by_z_compar(const void *av, const void *bv) {
 }
 
 void wm_sort_windows_by_z(struct wm_state *state) {
+    int mouse_wid = state->mouse_win->wid;
+
     qsort(state->windows, state->nwindows, sizeof(struct wm_window), wm_sort_windows_by_z_compar);
+
+    for (int i = 0; i < state->nwindows; i++) {
+        struct wm_window *win = &state->windows[i];
+        if (win->wid == mouse_wid) {
+            state->mouse_win = win;
+            break;
+        }
+    }
+}
+
+/* QUEUE */
+
+void wm_add_queue(struct wm_state *state, struct wm_atom *atom) {
+    if(state->queue_len == WM_EVENT_QUEUE_LEN) {
+        return;
+    }
+    state->queue[state->queue_len++] = *atom;
 }
 
 int main(int argc, char **argv) {
@@ -222,28 +261,67 @@ int main(int argc, char **argv) {
 
             unsigned int speed = __builtin_ffs(mouse_packet.x + mouse_packet.y);
             struct fbdev_bitblit *sprite = &wm.mouse_win->as.sprite.sprite;
+
+            struct wm_atom mouse_atom = { 0 };
+            mouse_atom.type = ATOM_MOUSE_EVENT_TYPE;
+
             if (mouse_packet.x != 0) {
                 // left = negative
                 sprite->x += mouse_packet.x * speed;
                 sprite->x = min(sprite->x, ws.ws_col);
                 wm.needs_redraw = 1;
             }
+            mouse_atom.mouse_event.x = sprite->x;
+
             if (mouse_packet.y != 0) {
                 // bottom = negative
                 sprite->y -= mouse_packet.y * speed;
                 sprite->y = min(sprite->y, ws.ws_col);
                 wm.needs_redraw = 1;
             }
+            mouse_atom.mouse_event.y = sprite->y;
+
+            if ((mouse_packet.attr_byte & MOUSE_ATTR_LEFT_BTN) != 0) {
+                mouse_atom.mouse_event.type = WM_MOUSE_PRESS;
+            } else {
+                mouse_atom.mouse_event.type = WM_MOUSE_RELEASE;
+            }
+
+            if(!wm_atom_eq(&mouse_atom, &wm.last_mouse_atom)) {
+                wm_add_queue(&wm, &mouse_atom);
+            }
+
+            wm.last_mouse_atom = mouse_atom;
         }
         for (int i = 0; i < wm.nwindows; i++) {
             struct wm_window *win = &wm.windows[i];
             switch (win->type) {
                 case WM_WINDOW_PROG: {
-                    struct wm_atom redraw_atom = {
-                        .type = ATOM_REDRAW_TYPE
-                    };
+                    int retval;
                     struct wm_atom respond_atom;
-                    int retval = win_write_and_wait(&win->as.prog, &redraw_atom, &respond_atom);
+
+                    // read from queue
+                    struct wm_atom req_atom;
+                    while(
+                        read(win->as.prog.sfd, (char *)&req_atom, sizeof(struct wm_atom))
+                            == sizeof(struct wm_atom)
+                    ) {
+                        wm_handle_request_atom(&wm, win, &req_atom);
+                    }
+
+                    // transmit events in queue
+                    for(int i = 0; i < wm.queue_len; i++) {
+                        if((win->as.prog.event_mask & (1 << wm.queue[i].type)) != 0) {
+                            win_write_and_wait(&win->as.prog, &wm.queue[i], &respond_atom);
+                        }
+                    }
+
+                    // request a redraw
+                    struct wm_atom redraw_atom = {
+                        .type = ATOM_REDRAW_TYPE,
+                        .redraw.force_redraw = wm.needs_redraw,
+                    };
+                    retval = win_write_and_wait(&win->as.prog, &redraw_atom, &respond_atom);
 
                     if (retval > 0) {
                         if (respond_atom.respond.retval)
@@ -257,12 +335,12 @@ int main(int argc, char **argv) {
                 }
             }
         }
-        ioctl(fb_fd, GFX_SWAPBUF, 0);
-        //if (wm.needs_redraw || 1) {
-        //    wm.needs_redraw = 0;
-        //}
-        // TODO: consistent frame rate
-        usleep(FRAME_TICK);
+        if (wm.needs_redraw) {
+            ioctl(fb_fd, GFX_SWAPBUF, 0);
+            wm.needs_redraw = 0;
+        }
+        wm.queue_len = 0;
+        usleep(FRAME_WAIT);
     }
 
     return 0;
