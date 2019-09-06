c79159cfbcc880743fa186d9aae0796ea5c4c088
ffwff <mogamiriver@tuta.io>
2019-09-06 17:36:18 +0700

    wm: trying to build an actual version

--- pkgs/wm/build.sh
+++ pkgs/wm/build.sh
@@ -7,7 +7,7 @@ _download() {
 build() {
     _download https://raw.githubusercontent.com/dhepper/font8x8/master/font8x8_basic.h "$build_dir/font8x8_basic.h"
 
-    ${opt_arch}-gcc -O2 -o $build_dir/wm $script_dir/wm.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lm -msse2
+    ${opt_arch}-gcc -Wall -O2 -o $build_dir/wm $script_dir/wm.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lm -msse2
     ${opt_arch}-gcc -O2 -o $build_dir/samplwin $script_dir/samplwin.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lm -msse2
     ${opt_arch}-gcc -O2 -o $build_dir/canvwin $script_dir/canvwin.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lm -msse2
     # ${opt_arch}-gcc -g -o $build_dir/cairowin $script_dir/cairowin.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -msse2 -lcairo -lpixman-1 -lm
