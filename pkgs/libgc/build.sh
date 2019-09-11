f65fb0c15f58078db8680591fc188bd000530bd1
ffwff <mogamiriver@tuta.io>
2019-09-11 18:39:45 +0700

    libgc: fix configure script

--- pkgs/libgc/build.sh
+++ pkgs/libgc/build.sh
@@ -1,13 +1,19 @@
+version=8.0.4
+
 build() {
-    if [[ ! -d "${build_dir}/gc-8.0.4" ]]; then
-        wget https://github.com/ivmai/bdwgc/releases/download/v8.0.4/gc-8.0.4.tar.gz
-        tar xf $build_dir/kilo
+    if [[ ! -d "${build_dir}/gc-$version" ]]; then
+        wget https://github.com/ivmai/bdwgc/releases/download/v$version/gc-$version.tar.gz
+        tar xf $build_dir/gc-$version
     fi
     pushd .
-        cd $build_dir/gc-8.0.4
+        cd $build_dir/gc-$version
         try_patch $script_dir/bdwgc.patch
         [[ $? -gt 1 ]] && exit 1
-        ../configure --target=$opt_arch --disable-mmap --enable-static || exit 1
-        make -j`nproc` || exit 1
+        ./configure --prefix=$opt_toolsdir --target=$opt_arch --disable-mmap --enable-static || exit 1
+        make -j`nproc` install
     popd
+}
+
+install() {
+    echo -ne
 }
