38c42bcb60767861812bc637754b122960fedb2d
ffwff <mogamiriver@tuta.io>
2019-09-23 21:05:50 +0700

    wm: add prototype file manager (fm), update cterm, pape
    - base: update cursor

--- userspace/applications/wm/cterm.c
+++ userspace/applications/wm/cterm.c
@@ -20,44 +20,44 @@
 #define LINE_BUFFER_LEN 128
 
 struct cterm_state {
-    int in_fd, out_fd;
+  int in_fd, out_fd;
 };
 
 void cterm_init(struct cterm_state *state) {
-    char path[128] = { 0 };
+  char path[128] = { 0 };
 
-    snprintf(path, sizeof(path), "/pipes/cterm:%d:in", getpid());
-    state->in_fd = create(path);
-    ioctl(state->in_fd, PIPE_CONFIGURE, PIPE_WAIT_READ);
+  snprintf(path, sizeof(path), "/pipes/cterm:%d:in", getpid());
+  state->in_fd = create(path);
+  ioctl(state->in_fd, PIPE_CONFIGURE, PIPE_WAIT_READ);
 
-    snprintf(path, sizeof(path), "/pipes/cterm:%d:out", getpid());
-    state->out_fd = create(path);
+  snprintf(path, sizeof(path), "/pipes/cterm:%d:out", getpid());
+  state->out_fd = create(path);
 }
 
 int main(int argc, char **argv) {
-    struct cterm_state state = { 0 };
-    cterm_init(&state);
+  struct cterm_state state = { 0 };
+  cterm_init(&state);
 
-    // spawn main
-    struct startup_info s_info = {
-        .stdin = state.in_fd,
-        .stdout = state.out_fd,
-        .stderr = state.out_fd,
-    };
-    char *spawn_argv[] = {"/hd0/main", NULL};
-    spawnxv(&s_info, "/hd0/main", (char **)spawn_argv);
+  // spawn main
+  struct startup_info s_info = {
+    .stdin = state.in_fd,
+    .stdout = state.out_fd,
+    .stderr = state.out_fd,
+  };
+  char *spawn_argv[] = {"/hd0/main", NULL};
+  spawnxv(&s_info, "/hd0/main", (char **)spawn_argv);
 
-    struct g_application *app = g_application_create(INIT_WIDTH, INIT_HEIGHT, 1);
-    g_application_set_userdata(app, &state);
-    
-    struct g_termbox *tb = g_termbox_create();
-    g_termbox_bind_in_fd(tb, state.in_fd);
-    g_termbox_bind_out_fd(tb, state.out_fd);
-    
-    struct g_window_layout *wlayout = g_window_layout_create((struct g_widget *)tb);
-    g_widget_move_resize((struct g_widget *)wlayout, 0, 0, INIT_WIDTH, INIT_HEIGHT);
-    g_decoration_set_text(g_window_layout_decoration(wlayout), "Terminal");
-    g_application_set_main_widget(app, (struct g_widget *)wlayout);
+  struct g_application *app = g_application_create(INIT_WIDTH, INIT_HEIGHT, 1);
+  g_application_set_userdata(app, &state);
+  
+  struct g_termbox *tb = g_termbox_create();
+  g_termbox_bind_in_fd(tb, state.in_fd);
+  g_termbox_bind_out_fd(tb, state.out_fd);
+  
+  struct g_window_layout *wlayout = g_window_layout_create((struct g_widget *)tb);
+  g_widget_move_resize((struct g_widget *)wlayout, 0, 0, INIT_WIDTH, INIT_HEIGHT);
+  g_decoration_set_text(g_window_layout_decoration(wlayout), "Terminal");
+  g_application_set_main_widget(app, (struct g_widget *)wlayout);
 
-    return g_application_run(app);
+  return g_application_run(app);
 }
--- userspace/applications/wm/fm.c
+++ userspace/applications/wm/fm.c
@@ -5,6 +5,7 @@
 #include <sys/ioctl.h>
 #include <sys/pipes.h>
 #include <syscalls.h>
+#include <dirent.h>
 
 #include <canvas.h>
 #include <wm/wmc.h>
@@ -20,37 +21,90 @@
 #define LINE_BUFFER_LEN 128
 
 struct fm_state {
-    char *path;
+  char path[128];
+  struct dirent files[256];
+  int nfiles;
+};
+
+static struct dirent up_dir = {
+  .d_name = ".."
 };
 
 static void fm_init(struct fm_state *state) {
+  getcwd(state->path, sizeof(state->path));
+
+  DIR *d = opendir(state->path);
+  state->files[0] = up_dir;
+  state->nfiles = 1;
+  struct dirent *dir;
+  while ((dir = readdir(d)) != NULL) {
+    state->files[state->nfiles++] = *dir;
+  }
+  closedir(d);
 }
 
-static void fm_redraw(struct g_widget *widget, struct g_application *app) {
+static int fm_redraw(struct g_widget *widget, struct g_application *app) {
+  if(g_widget_needs_redraw(widget)) {
     struct g_canvas *canvas = (struct g_canvas *)widget;
     struct canvas_ctx *ctx = g_canvas_ctx(canvas);
     
+    struct fm_state *state = g_application_userdata(app);
+    
     canvas_ctx_fill_rect(ctx, 0, 0,
-        g_widget_width(widget), g_widget_height(widget),
-        canvas_color_rgb(0x0, 0x0, 0x0));
-        
-    canvas_ctx_draw_text(ctx, 0, 0, "file");
+      g_widget_width(widget), g_widget_height(widget),
+      canvas_color_rgb(0x0, 0x0, 0x0));
+    
+    for(int i = 0; i < state->nfiles; i++) {
+      canvas_ctx_draw_text(ctx, 0, i * FONT_HEIGHT, state->files[i].d_name);
+    }
+    
+    g_widget_set_needs_redraw(widget, 0);
+    return 1;
+  }
+  
+  return 0;
+}
+
+static int address_redraw(struct g_widget *widget, struct g_application *app) {
+  if(g_widget_needs_redraw(widget)) {
+    struct g_canvas *canvas = (struct g_canvas *)widget;
+    struct canvas_ctx *ctx = g_canvas_ctx(canvas);
+    
+    struct fm_state *state = g_application_userdata(app);
+    
+    canvas_ctx_fill_rect(ctx, 0, 0,
+      g_widget_width(widget), g_widget_height(widget),
+      canvas_color_rgb(0x0, 0x0, 0x0));
+    
+    canvas_ctx_draw_text(ctx, 0, 0, state->path);
+
+    g_widget_set_needs_redraw(widget, 0);
+    return 1;
+  }
+
+  return 0;
 }
 
 int main(int argc, char **argv) {
-    struct fm_state state = { 0 };
-    fm_init(&state);
+  struct fm_state state = { 0 };
+  fm_init(&state);
 
-    struct g_application *app = g_application_create(INIT_WIDTH, INIT_HEIGHT, 1);
-    g_application_set_userdata(app, &state);
-    
-    struct g_canvas *main_widget = g_canvas_create();
-    g_canvas_set_redraw_fn(main_widget, fm_redraw);
+  struct g_application *app = g_application_create(INIT_WIDTH, INIT_HEIGHT, 1);
+  g_application_set_userdata(app, &state);
+  
+  struct g_canvas *address_widget = g_canvas_create();
+  g_widget_resize((struct g_widget *)address_widget, INIT_WIDTH - 10, 15);
+  g_canvas_set_redraw_fn(address_widget, address_redraw);
+  
+  struct g_canvas *main_widget = g_canvas_create();
+  g_canvas_set_redraw_fn(main_widget, fm_redraw);
 
-    struct g_window_layout *wlayout = g_window_layout_create((struct g_widget *)main_widget);
-    g_widget_move_resize((struct g_widget *)wlayout, 0, 0, INIT_WIDTH, INIT_HEIGHT);
-    g_decoration_set_text(g_window_layout_decoration(wlayout), "File Manager");
-    g_application_set_main_widget(app, (struct g_widget *)wlayout);
+  struct g_window_layout *wlayout = g_window_layout_create((struct g_widget *)main_widget);
+  g_widget_move_resize((struct g_widget *)wlayout, 0, 0, INIT_WIDTH, INIT_HEIGHT);
+  g_decoration_set_widget(g_window_layout_decoration(wlayout), (struct g_widget *)address_widget);
+  g_decoration_set_text(g_window_layout_decoration(wlayout), "File Manager");
+  g_widget_move_resize((struct g_widget *)wlayout, 0, 0, INIT_WIDTH, INIT_HEIGHT);
+  g_application_set_main_widget(app, (struct g_widget *)wlayout);
 
-    return g_application_run(app);
+  return g_application_run(app);
 }
