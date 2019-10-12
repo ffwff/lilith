40ecafbf6e643cdc930c84486f8fcdb996061692
ffwff <mogamiriver@tuta.io>
2019-10-12 11:48:03 +0700

    pape: don't load image incrementally

--- userspace/applications/wm/pape.c
+++ userspace/applications/wm/pape.c
@@ -11,19 +11,15 @@
 #include <gui.h>
 #include <png.h>
 
-#define min(x,y) ((x)<(y)?(x):(y))
-
 struct pape_state {
-  int drawn_rows;
   int drawn;
   int width, height;
   png_structp png_ptr;
   png_infop info_ptr;
-  unsigned char *row;
+  unsigned char *bg;
 };
 
 static void pape_init(struct pape_state *state) {
-  state->drawn_rows = 0;
   state->drawn = 0;
   state->width = 0;
   state->height = 0;
@@ -48,7 +44,7 @@ static void pape_init_img(struct pape_state *state, char *path) {
   
   state->width = width;
   state->height = height;
-  state->row = malloc(state->width * 4);
+  state->bg = malloc(state->width * state->height * 4);
   
   // Read any color_type into 8bit depth, RGBA format.
   // See http://www.libpng.org/pub/png/libpng-manual.txt
@@ -79,55 +75,47 @@ static void pape_init_img(struct pape_state *state, char *path) {
   png_set_bgr(png_ptr);
 
   png_read_update_info(png_ptr, info_ptr);
+
+  for(int y = 0; y < state->height; y++) {
+    png_read_row(png_ptr, &state->bg[y * state->width * 4], 0);
+  }
 }
 
-static void pape_redraw(struct g_application *app) {
+static int pape_redraw(struct g_application *app) {
   struct canvas_ctx *ctx = g_application_ctx(app);
   struct pape_state *state = (struct pape_state *)g_application_userdata(app);
   
   const int width = canvas_ctx_get_width(ctx);
-  const int max_height = canvas_ctx_get_height(ctx);
-  const int height = min(state->height, max_height);
   
-  if(state->drawn_rows < height) {
+  if(!state->drawn) {
     unsigned char *dst = canvas_ctx_get_surface(ctx);
-    png_read_row(state->png_ptr, state->row, 0);
-    memcpy(&dst[state->drawn_rows * width * 4], state->row, state->width * 4);
-    state->drawn_rows++;
-  } else if (state->drawn_rows == height) {
-    g_application_clear_timeout(app);
+    for(int y = 0; y < state->height; y++) {
+      memcpy(&dst[y * width * 4], &state->bg[y * state->width * 4], state->width * 4);
+    }
     state->drawn = 1;
   }
-}
 
-static int window_redraw(struct g_application *app) {
-  struct pape_state *state = (struct pape_state *)g_application_userdata(app);
-  if(state->drawn) {
-    state->drawn = 0;
-    return 1;
-  }
-  return 0;
+  return 1;
 }
 
 int main(int argc, char **argv) {
-  struct pape_state state = { 0 };
-  pape_init(&state);
-  
   if(argc != 2) {
     printf("usage: %s wallpaper\n", argv[0]);
     return 1;
   }
+  struct pape_state state = { 0 };
+  pape_init(&state);
   pape_init_img(&state, argv[1]);
-  
+
   struct g_application *app = g_application_create(1, 1, 0);
-  g_application_set_timeout_cb(app, 0, pape_redraw);
-  g_application_set_event_mask(app, 0);
   g_application_set_window_properties(app, WM_PROPERTY_ROOT);
   g_application_set_userdata(app, &state);
   int width, height;
   g_application_screen_size(app, &width, &height);
   g_application_resize(app, width, height);
   
-  g_application_set_redraw_cb(app, window_redraw);
+  g_application_set_redraw_cb(app, pape_redraw);
   return g_application_run(app);
 }
+
+
