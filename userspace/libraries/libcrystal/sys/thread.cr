9351bcf51f7e9890c93aa465669f743cafb0631e
ffwff <mogamiriver@tuta.io>
2019-10-13 13:53:17 +0700

    libcrystal: correct sleep timeout

--- userspace/libraries/libcrystal/sys/thread.cr
+++ userspace/libraries/libcrystal/sys/thread.cr
@@ -1,10 +1,10 @@
 lib LibC
-  fun usleep(timeout : LibC::UInt)
+  fun usleep(timeout : LibC::UInt) : LibC::Int
   fun exit(code : LibC::Int) : NoReturn
 end
 
 def sleep(seconds : Int)
-  usecs = seconds.to_uint * 1000
+  usecs = seconds.to_uint * 1000000
   LibC.usleep usecs
 end
 
