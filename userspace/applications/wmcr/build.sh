f40bc9591d9117a33de8819bf68d5fd82f1dd0fd
ffwff <mogamiriver@tuta.io>
2019-10-23 20:45:56 +0700

    wmcr: test out ipc

--- userspace/applications/wmcr/build.sh
+++ userspace/applications/wmcr/build.sh
@@ -1,9 +1,10 @@
 build() {
   LDFLAGS="-I$opt_toolsdir/include -L$opt_toolsdir/lib -lpng -lz -lm -msse2" $script_dir/compile $script_dir/wmcr.cr $build_dir/wmcr
+  $script_dir/compile $script_dir/windem.cr $build_dir/windem
 }
 
 install() {
-    for i in $script_dir/*.cr; do
-        sudo cp $build_dir/$(basename $i .cr) $install_dir/bin
-    done
+  for i in $script_dir/*.cr; do
+    sudo cp $build_dir/$(basename $i .cr) $install_dir/bin
+  done
 }
--- userspace/applications/wmcr/windem.cr
+++ userspace/applications/wmcr/windem.cr
@@ -1,1 +1,4 @@
 require "./wm/*"
+
+client = Wm::Client.new.not_nil!
+client << Wm::IPC.test_message
