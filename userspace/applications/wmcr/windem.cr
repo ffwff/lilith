075eed6f645d6287c9cb2895ad0901be9c309cf6
ffwff <mogamiriver@tuta.io>
2019-10-24 14:29:31 +0700

    wmcr: add window create message
    - libcrystal: add Slice#+

--- userspace/applications/wmcr/windem.cr
+++ userspace/applications/wmcr/windem.cr
@@ -1,4 +1,5 @@
 require "./wm/*"
 
 client = Wm::Client.new.not_nil!
-client << Wm::IPC.test_message
+client << Wm::IPC.window_create_message(420,420,420,420)
+# client << Wm::IPC.test_message
--- userspace/applications/wmcr/wm/ipc.cr
+++ userspace/applications/wmcr/wm/ipc.cr
@@ -9,8 +9,18 @@ module Wm::IPC
       length : UInt8
       type : UInt8
     end
+
+    TEST_MESSAGE_ID = 0
+
+    WINDOW_CREATE_ID = 1
+    @[Packed]
+    struct WindowCreate
+      header : Header
+      x, y, width, height : UInt32
+    end
   end
 
+  # Checks if bytes represents a valid IPC message
   def valid_msg?(msg : Bytes)
     return false if msg.size < sizeof(Data::Header)
     header = msg.to_unsafe.as(Data::Header*)
@@ -22,6 +32,7 @@ module Wm::IPC
     sizeof(Data::Header) + header.value.length <= msg.size
   end
 
+  # Creates IPC header
   def create_header(length, type)
     header = Data::Header.new
     LibC.strncpy(header.magic.to_unsafe,
@@ -32,13 +43,39 @@ module Wm::IPC
     header
   end
 
+  # Gets a pointer to the header of a byte stream
   def header_part(msg : Bytes)
     msg.to_unsafe.as(Data::Header*)
   end
 
+  # Gets a pointer to the payload of a byte stream
+  macro payload_part(t, msg)
+    ({{ msg }}.to_unsafe).as({{ t }}*).value
+  end
+
+  # Creates a test message
   def test_message
     msg = uninitialized UInt8[sizeof(Data::Header)]
-    msg.to_unsafe.as(Data::Header*).value = create_header 0, 0
+    msg.to_unsafe.as(Data::Header*)
+      .value = create_header 0, Data::TEST_MESSAGE_ID
+    msg
+  end
+
+  private macro payload_size(t)
+    sizeof({{ t }}) - sizeof(Data::Header)
+  end
+
+  # Creates window create message
+  def window_create_message(x, y, width, height)
+    msg = uninitialized UInt8[sizeof(Data::WindowCreate)]
+    wc = msg.to_unsafe.as(Data::WindowCreate*)
+    wc.value.header = create_header(
+      payload_size(Data::WindowCreate),
+      Data::WINDOW_CREATE_ID)
+    wc.value.x = x
+    wc.value.y = y
+    wc.value.width = width
+    wc.value.height = height
     msg
   end
 
