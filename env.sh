c2f72d64fa0d2078cdef81f65dbe3489668780fe
ffwff <mogamiriver@tuta.io>
2019-10-27 19:46:34 +0700

    libcrystal: fix buffer overflow on Array#[]?

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-if [[ "$LILITH_ENV" -eq 1 ]]; then
-  exit
-fi
+if [[ "$LILITH_ENV" -ne 1 ]]; then
 export LILITH_ENV=1
 export PATH="$(pwd)/userspace/toolchain/tools/bin:$(pwd)/toolchain/crystal/.build:$PATH"
+fi
--- userspace/libraries/libcrystal/core/array.cr
+++ userspace/libraries/libcrystal/core/array.cr
@@ -99,7 +99,7 @@ class Array(T)
   end
 
   def []?(idx : Int) : T?
-    return nil unless 0 <= idx && idx <= size
+    return nil unless 0 <= idx && idx < size
     to_unsafe[idx]
   end
 
