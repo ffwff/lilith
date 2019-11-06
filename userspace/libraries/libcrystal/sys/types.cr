58dced932e769d1437780974beced153854739fe
ffwff <mogamiriver@tuta.io>
2019-11-06 18:19:17 +0700

    libcrystal: fix SizeT/Long typing for 64-bit systems

--- userspace/libraries/libcrystal/sys/types.cr
+++ userspace/libraries/libcrystal/sys/types.cr
@@ -3,14 +3,22 @@ lib LibC
   alias UString = UInt8*
   alias Pid = Int32
 
-  alias SizeT = UInt32
-  alias SSizeT = Int32
   alias Int = Int32
   alias UInt = UInt32
-  alias Long = Int32
-  alias ULong = UInt32
   alias LongLong = Int64
   alias ULongLong = UInt64
+
+  {% if flag?(:bits32) %}
+    alias SizeT = UInt32
+    alias SSizeT = Int32
+    alias Long = Int32
+    alias ULong = UInt32
+  {% else %}
+    alias SizeT = UInt64
+    alias SSizeT = Int64
+    alias Long = Int64
+    alias ULong = UInt64
+  {% end %}
 end
 
 struct Int
@@ -22,7 +30,13 @@ struct Int
     self.to_u32
   end
 
-  def to_usize
-    self.to_u32
-  end
+  {% if flag?(:bits32) %}
+    def to_usize
+      self.to_u32
+    end
+  {% else %}
+    def to_usize
+      self.to_u32
+    end
+  {% end %}
 end
