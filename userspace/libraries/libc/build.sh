3a2592f88888d2d70cd34c56f88bef147637e1e1
ffwff <mogamiriver@tuta.io>
2019-11-08 17:01:01 +0700

    libc: fix build scripts, add rcx/rdx to clobber list

--- userspace/libraries/libc/build.sh
+++ userspace/libraries/libc/build.sh
@@ -1,7 +1,7 @@
 build() {
     pushd .
         cd $script_dir && \
-        make ARCH=$opt_arch RELEASE=1 -B -j`nproc` && \
+        make ARCH=$opt_arch RELEASE=1 -B -j`nproc` libc.a libm.a && \
         make install \
             LIBDIR=$opt_toolsdir/lib/gcc/$opt_arch/8.3.0/ \
             INCLUDEDIR=$opt_toolsdir/lib/gcc/$opt_arch/8.3.0/include
