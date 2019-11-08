9cae3a082e9bb0b15a9dba005c6fdde37b26bee4
ffwff <mogamiriver@tuta.io>
2019-11-08 17:02:15 +0700

    kernel: clean up driver_thread, remove kswitch_usermode32

--- src/asm/x64/user.s
+++ src/asm/x64/user.s
@@ -1,13 +1,2 @@
 USER_RFLAGS = 0x212
 USER_DATA_SELECTOR = 0x23
-
-.section .text
-.global kswitch_usermode32
-kswitch_usermode32:
-    mov %rsp, %rbp
-    mov $USER_DATA_SELECTOR, %rax
-    mov %ax, %ds
-    mov %ax, %fs
-    mov %ax, %gs
-    mov $USER_RFLAGS, %r11
-    sysret
--- src/multiprocessing/driver_thread.cr
+++ src/multiprocessing/driver_thread.cr
@@ -1,31 +1,29 @@
-module Multiprocessing
-  module DriverThread
-    extend self
+module Multiprocessing::DriverThread
+  extend self
 
+  @@locked = false
+
+  def unlock
     @@locked = false
+    # Serial.puts "driverthread: unlock\n"
+  end
 
-    def unlock
-      @@locked = false
-      # Serial.puts "driverthread: unlock\n"
+  def lock
+    @@locked = true
+    # Serial.puts "driverthread: lock \n"
+  end
+
+  def assert_unlocked
+    if Multiprocessing::Scheduler.current_process.nil?
+      return
     end
-
-    def lock
-      @@locked = true
-      # Serial.puts "driverthread: lock \n"
+    unless Multiprocessing::Scheduler.current_process.not_nil!.kernel_process?
+      return
     end
-
-    def assert_unlocked
-      if Multiprocessing::Scheduler.current_process.nil?
-        return
-      end
-      unless Multiprocessing::Scheduler.current_process.not_nil!.kernel_process?
-        return
-      end
-      # Serial.puts "assert_unlocked called from ", Multiprocessing.current_process.not_nil!.name, '\n'
-      if @@locked
-        name = Multiprocessing::Scheduler.current_process.not_nil!.name
-        panic "kernel: subsystem must not be called from driver thread (called by ", name, ")\n"
-      end
+    # Serial.puts "assert_unlocked called from ", Multiprocessing.current_process.not_nil!.name, '\n'
+    if @@locked
+      name = Multiprocessing::Scheduler.current_process.not_nil!.name
+      panic "kernel: subsystem must not be called from driver thread (called by ", name, ")\n"
     end
   end
 end
