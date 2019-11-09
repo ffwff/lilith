7bdb724fca698285db626cfe28029b8ef0810536
ffwff <mogamiriver@tuta.io>
2019-11-09 16:56:52 +0700

    kernel: add class variants of getter, setter, property macros

--- src/core/object.cr
+++ src/core/object.cr
@@ -3,53 +3,58 @@ class Object
     self
   end
 
-  macro getter(*names)
-    {% for name in names %}
-      {% if name.is_a?(TypeDeclaration) %}
-        def {{ name.var.id }} : {{name.type}}
-          @{{ name.var.id }}
-        end
-      {% else %}
-        def {{ name.id }}
-          @{{ name.id }}
-        end
-      {% end %}
-    {% end %}
-  end
+  {% for prefixes in { {"", "", "@", "#"}, {"class_", "self.", "@@", "."} } %}
+    {%
+      macro_prefix = prefixes[0].id
+      method_prefix = prefixes[1].id
+      var_prefix = prefixes[2].id
+      doc_prefix = prefixes[3].id
+    %}
+    macro {{macro_prefix}}getter(*names)
+      \{% for name in names %}
+        \{% if name.is_a?(TypeDeclaration) %}
+          def {{method_prefix}}\{{ name.var.id }} : \{{name.type}}
+            {{var_prefix}}\{{ name.var.id }}
+          end
+        \{% else %}
+          def {{method_prefix}}\{{ name.id }}
+            {{var_prefix}}\{{ name.id }}
+          end
+        \{% end %}
+      \{% end %}
+    end
 
-  macro getter!(*names)
-    {% for name in names %}
-      {% if name.is_a?(TypeDeclaration) %}
-        def {{ name.var.id }} : {{name.type}}
-          @{{ name.var.id }}.not_nil!
-        end
-      {% else %}
-        def {{ name.id }}
-          @{{ name.id }}.not_nil!
-        end
-      {% end %}
-    {% end %}
-  end
+    macro {{macro_prefix}}getter!(*names)
+      \{% for name in names %}
+        \{% if name.is_a?(TypeDeclaration) %}
+          def {{method_prefix}}\{{ name.var.id }} : \{{name.type}}
+            {{var_prefix}}\{{ name.var.id }}.not_nil!
+          end
+        \{% else %}
+          def {{method_prefix}}\{{ name.id }}
+            {{var_prefix}}\{{ name.id }}.not_nil!
+          end
+        \{% end %}
+      \{% end %}
+    end
 
-  macro property(*names)
-    {% for name in names %}
-    def {{ name.id }}
-      @{{ name.id }}
+    macro {{macro_prefix}}setter(*names)
+      \{% for name in names %}
+        def {{method_prefix}}\{{ name.id }}=({{var_prefix}}\{{ name.id }})
+        end
+      \{% end %}
     end
-    def {{ name.id }}=(@{{ name.id }})
-    end
-    {% end %}
-  end
 
-  macro mod_property(*names)
-    {% for name in names %}
-    def {{ name.id }}
-      @@{{ name.id }}
+    macro {{macro_prefix}}property(*names)
+      \{% for name in names %}
+        def {{method_prefix}}\{{ name.id }}
+          {{var_prefix}}\{{ name.id }}
+        end
+        def {{method_prefix}}\{{ name.id }}=({{var_prefix}}\{{ name.id }})
+        end
+      \{% end %}
     end
-    protected def {{ name.id }}=(@@{{ name.id }})
-    end
-    {% end %}
-  end
+  {% end %}
 
   def unsafe_as(type : T.class) forall T
     x = self
