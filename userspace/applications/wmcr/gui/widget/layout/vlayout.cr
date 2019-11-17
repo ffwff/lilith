9fe44824d6ebb89da5af85be19344adb32ef6407
ffwff <mogamiriver@tuta.io>
2019-11-17 08:36:20 +0700

    wmcr: handle stretch widget for vlayout

--- userspace/applications/wmcr/gui/widget/layout/vlayout.cr
+++ userspace/applications/wmcr/gui/widget/layout/vlayout.cr
@@ -1,11 +1,58 @@
 class G::VLayout < G::Layout
 
   @placement_x = 0
+  @has_stretch = false
 
   def add_widget(widget : G::Widget)
-    widget.move @placement_x, widget.y
-    @placement_x += widget.width
-    @widgets.push widget
+    if @has_stretch
+      # calculate widths for stretches
+      widgets_width = 0 
+      n_stretch = 0
+      @widgets.each do |w|
+        case w
+        when G::Stretch
+          n_stretch += 1
+        else
+          widgets_width += w.width
+        end
+      end
+      case widget
+      when G::Stretch
+        n_stretch += 1
+      else
+        widgets_width += widget.width
+      end
+
+      # place the widgets
+      stretch_width = (@width - widgets_width) // n_stretch
+      placement_x = 0
+      @widgets.each do |w|
+        case w
+        when G::Stretch
+          placement_x += stretch_width
+        else
+          w.move placement_x, w.y
+          placement_x += w.width
+        end
+      end
+
+      case widget
+      when G::Stretch
+      else
+        widget.move placement_x, widget.y
+      end
+      @widgets.push widget
+    else
+      case widget
+      when G::Stretch
+        @has_stretch = true
+      else
+        widget.move @placement_x, widget.y
+        @placement_x += widget.width
+      end
+      @widgets.push widget
+    end
+
   end
 
 end
