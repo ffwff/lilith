762a6b72dcad360bcc5e4e729a5aaba6d01c6ec0
ffwff <mogamiriver@tuta.io>
2019-11-18 22:40:48 +0700

    kernel: fix clobber for memcpy/memset

--- src/core/mem.cr
+++ src/core/mem.cr
@@ -1,7 +1,20 @@
 fun memset(dst : UInt8*, c : USize, n : USize) : Void*
+  r0 = r1 = r2 = 0
   asm(
     "cld\nrep stosb"
-          :: "{al}"(c.to_u8), "{rdi}"(dst), "{rcx}"(n)
+          : "={al}"(r0), "={Di}"(r1), "={cx}"(r2)
+          : "{al}"(c.to_u8), "{Di}"(dst), "{cx}"(n)
+          : "volatile", "memory"
+  )
+  dst.as(Void*)
+end
+
+fun memcpy(dst : UInt8*, src : UInt8*, n : USize) : Void*
+  r0 = r1 = r2 = 0
+  asm(
+    "cld\nrep movsb"
+          : "={Di}"(r0), "={Si}"(r1), "={cx}"(r2)
+          : "{Di}"(dst), "{Si}"(src), "{cx}"(n)
           : "volatile", "memory"
   )
   dst.as(Void*)
@@ -9,25 +22,13 @@ end
 
 @[AlwaysInline]
 def memset_long(dst : UInt32*, c : UInt32, n : USize)
+  r0 = r1 = r2 = 0
   asm(
     "cld\nrep stosl"
-          :: "{eax}"(c), "{rdi}"(dst), "{rcx}"(n)
+          : "={eax}"(r0), "={Di}"(r1), "={cx}"(r2)
+          : "{eax}"(c), "{Di}"(dst), "{cx}"(n)
           : "volatile", "memory"
   )
-end
-
-fun memcpy(dst : UInt8*, src : UInt8*, n : USize) : Void*
-  asm(
-    "shrq $$3, %rcx
-    andl $$7, %edx
-    cld
-    rep movsq
-    movl %edx, %ecx
-    rep movsb"
-          :: "{rdi}"(dst), "{rsi}"(src), "{rcx}"(n)
-          : "volatile", "memory"
-  )
-  dst.as(Void*)
 end
 
 fun memcmp(s1 : UInt8*, s2 : UInt8*, n : Int32) : Int32
