2148b54a69b50ff7ba81f7e3e1ae804501d960a3
ffwff <mogamiriver@tuta.io>
2019-11-19 14:55:51 +0700

    os: remove libgui/libwm/wm packages, rename wmcr to wm

--- userspace/applications/wm/build.sh
+++ userspace/applications/wm/build.sh
@@ -1,22 +1,14 @@
-_download() {
-    if [[ ! -f "$2" ]]; then
-        wget -O"$2" "$1"
-    fi
-}
-
 build() {
-    _download https://raw.githubusercontent.com/dhepper/font8x8/master/font8x8_basic.h "$build_dir/font8x8_basic.h"
-
-    ${opt_arch}-gcc -O2 -g -o $build_dir/wm $script_dir/wm.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lm -msse2
-    ${opt_arch}-gcc -g -o $build_dir/desktop $script_dir/desktop.c -I$opt_toolsdir/include -L$opt_toolsdir/lib
-    ${opt_arch}-gcc -g -o $build_dir/cterm $script_dir/cterm.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lm -lgui -lcanvas -lwmc -msse2
-    ${opt_arch}-gcc -g -o $build_dir/cbar $script_dir/cbar.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lm -lgui -lcanvas -lwmc -msse2
-    ${opt_arch}-gcc -g -o $build_dir/fm $script_dir/fm.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lgui -lcanvas -lwmc -lm  -msse2
-    ${opt_arch}-gcc -g -o $build_dir/pape $script_dir/pape.c -I$opt_toolsdir/include -L$opt_toolsdir/lib -lgui -lcanvas -lwmc -lpng -lz -lm  -msse2
+  LDFLAGS="-I$opt_toolsdir/include -L$opt_toolsdir/lib $script_dir/painter/c/alpha_blend.c -lpng -lz -lm -msse2" $script_dir/compile $script_dir/wmcr.cr $build_dir/wmcr
+  $script_dir/compile $script_dir/windem.cr $build_dir/windem
+  $script_dir/compile $script_dir/desktop.cr $build_dir/desktop
+  $script_dir/compile $script_dir/cterm.cr $build_dir/cterm
+  $script_dir/compile $script_dir/cbar.cr $build_dir/cbar
+  LDFLAGS="-I$opt_toolsdir/include -L$opt_toolsdir/lib -lpng -lz -lm -msse2" $script_dir/compile $script_dir/pape.cr $build_dir/pape
 }
 
 install() {
-    for i in $script_dir/*.c; do
-        sudo cp $build_dir/$(basename $i .c) $install_dir/bin
-    done
+  for i in $script_dir/*.cr; do
+    sudo cp $build_dir/$(basename $i .cr) $install_dir/bin
+  done
 }
