b224938fc98b6f699e35a74900b5ae73effa7203
ffwff <mogamiriver@tuta.io>
2019-11-19 14:42:40 +0700

    wmcr: send refocus event on new window, add startup delay to desktop

--- userspace/applications/wmcr/cbar.cr
+++ userspace/applications/wmcr/cbar.cr
@@ -33,6 +33,7 @@ Bar.window = window
 
 lbox = G::LayoutBox.new 0, 0, w, 10
 window.main_widget = lbox
+
 lbox.layout = G::VLayout.new lbox
 lbox.add_widget G::Label.new(0, 3, "lilith")
 lbox.add_widget G::Stretch.new
