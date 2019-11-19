e60c31777b58dd77a06a73edc52dfbc1683f182e
ffwff <mogamiriver@tuta.io>
2019-11-19 13:28:32 +0700

    kernel: fix registers for zero_page

--- src/arch/fastmem.cr
+++ src/arch/fastmem.cr
@@ -1,7 +1,9 @@
 def zero_page(mem : UInt8*, npages : USize = 1)
   return if npages == 0
   count = npages * 0x200
+  r0 = r1 = r2 = 0
   asm("cld\nrep stosq"
-          :: "{rax}"(0), "{rdi}"(mem), "{rcx}"(count)
+          : "={ax}"(r0), "={Di}"(r1), "={cx}"(r2)
+          : "{ax}"(0), "{Di}"(mem), "{cx}"(count)
           : "volatile", "memory")
 end
