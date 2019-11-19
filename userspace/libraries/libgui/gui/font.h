c93d906688e45e3382b2995ea872ccc6c355cf0b
ffwff <mogamiriver@tuta.io>
2019-11-19 14:55:51 +0700

    os: remove libgui/libwm/wm packages, rename wmcr to wm

--- userspace/libraries/libgui/gui/font.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern char font8x8_basic[128][8];
-
-#define FONT_WIDTH 8
-#define FONT_HEIGHT 8
-
-void canvas_ctx_draw_character(struct canvas_ctx *ctx, int xs, int ys, const char ch);
-void canvas_ctx_draw_text(struct canvas_ctx *ctx, int xs, int ys, const char *s);
