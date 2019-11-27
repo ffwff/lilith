002ced4547006257561870d2a10237f9831c89f3
ffwff <mogamiriver@tuta.io>
2019-11-27 17:14:52 +0700

    libc: fix setjmp structure

--- userspace/libraries/libc/src/include/setjmp.h
+++ userspace/libraries/libc/src/include/setjmp.h
@@ -1,11 +1,19 @@
 #ifndef _LIBC_SETJMP_H
 #define _LIBC_SETJMP_H
 
-struct __jmp_buf {
-    unsigned long registers[6];
-};
+#ifdef __i386__
+typedef unsigned long __jmp_buf[6];
+#endif
+#ifdef __x86_64__
+typedef unsigned long __jmp_buf[8];
+#endif
 
-typedef struct __jmp_buf jmp_buf[1];
+typedef struct __jmp_buf_tag {
+	__jmp_buf __jb;
+	unsigned long __fl;
+	unsigned long __ss[128/sizeof(long)];
+} jmp_buf[1];
+
 int setjmp(jmp_buf env);
 void longjmp(jmp_buf env, int value);
 
