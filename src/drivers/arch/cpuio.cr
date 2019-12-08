27f2b7b3cf69fd8889a7d1026722bc031894b227
ffwff <mogamiriver@tuta.io>
2019-12-08 21:29:33 +0700

    kernel: fix clobber values for cpuio functions

--- src/drivers/arch/cpuio.cr
+++ src/drivers/arch/cpuio.cr
@@ -2,32 +2,32 @@ module X86
   extend self
 
   def outb(port : UInt16, val : UInt8)
-    asm("outb $1, $0" :: "{dx}"(port), "{al}"(val) :: "volatile")
+    asm("outb $1, $0" :: "{dx}"(port), "{al}"(val) : "volatile")
   end
 
   def outw(port : UInt16, val : UInt16)
-    asm("outw $1, $0" :: "{dx}"(port), "{ax}"(val) :: "volatile")
+    asm("outw $1, $0" :: "{dx}"(port), "{ax}"(val) : "volatile")
   end
 
   def outl(port : UInt16, val : UInt32)
-    asm("outl $1, $0" :: "{dx}"(port), "{eax}"(val) :: "volatile")
+    asm("outl $1, $0" :: "{dx}"(port), "{eax}"(val) : "volatile")
   end
 
   def inb(port : UInt16) : UInt8
     result = 0_u8
-    asm("inb $1, $0" : "={al}"(result) : "{dx}"(port) :: "volatile")
+    asm("inb $1, $0" : "={al}"(result) : "{dx}"(port) : "volatile")
     result
   end
 
   def inw(port : UInt16) : UInt16
     result = 0_u16
-    asm("inw $1, $0" : "={ax}"(result) : "{dx}"(port) :: "volatile")
+    asm("inw $1, $0" : "={ax}"(result) : "{dx}"(port) : "volatile")
     result
   end
 
   def inl(port : UInt16) : UInt32
     result = 0_u32
-    asm("inl $1, $0" : "={eax}"(result) : "{dx}"(port) :: "volatile")
+    asm("inl $1, $0" : "={eax}"(result) : "{dx}"(port) : "volatile")
     result
   end
 end
