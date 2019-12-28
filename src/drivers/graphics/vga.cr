da31794c781343f8ffe9042f5bf164bd21dff889
ffwff <mogamiriver@tuta.io>
2019-12-28 12:33:51 +0700

    kernel: VGADevice => VGA

--- src/drivers/graphics/vga.cr
+++ src/drivers/graphics/vga.cr
@@ -2,7 +2,7 @@ VGA_WIDTH  = 80
 VGA_HEIGHT = 25
 VGA_SIZE   = VGA_WIDTH * VGA_HEIGHT
 
-module VGADevice
+module VGA
   extend self
 
   private module Unlocked
@@ -10,8 +10,8 @@ module VGADevice
 
     @@cx = 0
     @@cy = 0
-    @@fg = VGADevice::Color::White
-    @@bg = VGADevice::Color::Black
+    @@fg = VGA::Color::White
+    @@bg = VGA::Color::Black
     class_property cx
     class_property cy
     class_property fg
@@ -50,10 +50,118 @@ module VGADevice
       @@cy = VGA_HEIGHT - 1
     end
 
-    @@buffer = Pointer(UInt16).new(0xb8000u64 | Paging::IDENTITY_MASK)
+    protected def color_code(fg : VGA::Color, bg : VGA::Color, char : UInt8) : UInt16
+      attrib = (bg.value << 4) | fg.value
+      (attrib << 8) | char.to_u8
+    end
 
-    def buffer
-      @@buffer
+    protected def offset(x : Int, y : Int)
+      y * VGA_WIDTH + x
+    end
+
+    @@buffer = Pointer(UInt16).new(0xb8000u64 | Paging::IDENTITY_MASK)
+    class_getter buffer
+
+    def init_device
+      enable_cursor 0, 1
+      # fill with blank
+      blank = color_code VGA::Color::White, VGA::Color::Black, ' '.ord.to_u8
+      VGA_HEIGHT.times do |y|
+        VGA_WIDTH.times do |x|
+          @@buffer[offset x, y] = blank
+        end
+      end
+    end
+
+    def scroll
+      blank = color_code @@fg, @@bg, ' '.ord.to_u8
+      (VGA_HEIGHT - 1).times do |y|
+        VGA_WIDTH.times do |x|
+          @@buffer[offset x, y] = @@buffer[offset x, (y + 1)]
+        end
+      end
+      VGA_WIDTH.times do |x|
+        @@buffer[VGA_SIZE - VGA_WIDTH + x] = blank
+      end
+      wrapback
+    end
+
+    private def putc(x : Int32, y : Int32, fg : VGA::Color, bg : VGA::Color, a : UInt8)
+      abort "drawing out of bounds (80x25)!" if x > VGA_WIDTH || y > VGA_HEIGHT
+      @@buffer[offset x, y] = color_code(fg, bg, a)
+    end
+
+    private def putchar(ch : UInt8)
+      if ch == '\r'.ord.to_u8
+        return
+      elsif ch == '\n'.ord.to_u8
+        newline
+        return
+      elsif ch == 8u8
+        backspace
+        putc(cx, cy, fg, bg, ' '.ord.to_u8)
+        return
+      end
+      if cy >= VGA_HEIGHT
+        scroll
+      end
+      putc(cx, cy, fg, bg, ch)
+      advance
+    end
+
+    def putc(ch : UInt8)
+      if @@ansi_handler.nil?
+        putchar(ch)
+      else
+        seq = @@ansi_handler.parse ch
+        case seq
+        when AnsiHandler::CsiSequence
+          case seq.type
+          when AnsiHandler::CsiSequenceType::EraseInLine
+            blank = color_code @@fg, @@bg, ' '.ord.to_u8
+            if seq.arg_n == 0 && @@cy < VGA_HEIGHT - 1
+              x = @@cx
+              while x < VGA_WIDTH
+                @@buffer[offset x, @@cy] = blank
+                x += 1
+              end
+            end
+          when AnsiHandler::CsiSequenceType::MoveCursor
+            @@cx = Math.clamp(seq.arg_m.not_nil!.to_i32 - 1, 0, VGA_WIDTH)
+            @@cy = Math.clamp(seq.arg_n.not_nil!.to_i32 - 1, 0, VGA_HEIGHT)
+            move_cursor @@cx, @@cy
+          end
+        when UInt8
+          putchar(seq)
+        end
+      end
+    end
+
+    # Cursor
+    def enable_cursor(cursor_start, cursor_end)
+      X86.outb(0x3D4, 0x0A)
+      X86.outb(0x3D5, (X86.inb(0x3D5) & 0xC0) | cursor_start)
+      X86.outb(0x3D4, 0x0B)
+      X86.outb(0x3D5, (X86.inb(0x3D5) & 0xE0) | cursor_end)
+    end
+
+    def disable_cursor
+      X86.outb(0x3D4, 0x0A)
+      X86.outb(0x3D5, 0x20)
+    end
+
+    def move_cursor(x, y)
+      pos = offset x, y
+      X86.outb(0x3D4, 0x0F)
+      X86.outb(0x3D5, (pos & 0xFF).to_u8)
+      X86.outb(0x3D4, 0x0E)
+      X86.outb(0x3D5, ((pos >> 8) & 0xFF).to_u8)
+    end
+  end
+
+  def init_device
+    lock do |state|
+      state.init_device
     end
   end
 
@@ -88,128 +196,18 @@ module VGADevice
     White      = 15
   end
 
-  struct Instance < OutputDriver
-    private def color_code(fg : VGADevice::Color, bg : VGADevice::Color, char : UInt8) : UInt16
-      attrib = (bg.value << 4) | fg.value
-      (attrib << 8) | char.to_u8
+  def putc(ch : UInt8)
+    lock do |state|
+      state.putc ch
     end
+  end
 
-    private def offset(x : Int, y : Int)
-      y * VGA_WIDTH + x
+  def print(*args)
+    args.each do |arg|
+      arg.to_s self
     end
-
-    def initialize
-      enable_cursor 0, 1
-      # fill with blank
-      blank = color_code VGADevice::Color::White, VGADevice::Color::Black, ' '.ord.to_u8
-      VGADevice.lock do |state|
-        VGA_HEIGHT.times do |y|
-          VGA_WIDTH.times do |x|
-            state.buffer[offset x, y] = blank
-          end
-        end
-      end
-    end
-
-    # must be called from putc(ch)
-    private def putc(state, x : Int32, y : Int32, fg : VGADevice::Color, bg : VGADevice::Color, a : UInt8)
-      abort "drawing out of bounds (80x25)!" if x > VGA_WIDTH || y > VGA_HEIGHT
-      state.buffer[offset x, y] = color_code(fg, bg, a)
-    end
-
-    # must be called from putc(ch)
-    private def putchar(state, ch : UInt8)
-      if ch == '\r'.ord.to_u8
-        return
-      elsif ch == '\n'.ord.to_u8
-        state.newline
-        return
-      elsif ch == 8u8
-        state.backspace
-        putc(state, state.cx, state.cy, state.fg, state.bg, ' '.ord.to_u8)
-        return
-      end
-      if state.cy >= VGA_HEIGHT
-        scroll state
-      end
-      putc(state, state.cx, state.cy, state.fg, state.bg, ch)
-      state.advance
-    end
-
-    def putc(ch : UInt8)
-      VGADevice.lock do |state|
-        ansi_handler = state.ansi_handler
-        if ansi_handler.nil?
-          putchar(state, ch)
-        else
-          seq = ansi_handler.parse ch
-          case seq
-          when AnsiHandler::CsiSequence
-            case seq.type
-            when AnsiHandler::CsiSequenceType::EraseInLine
-              blank = color_code state.fg, state.bg, ' '.ord.to_u8
-              if seq.arg_n == 0 && state.cy < VGA_HEIGHT - 1
-                x = state.cx
-                while x < VGA_WIDTH
-                  state.buffer[offset x, state.cy] = blank
-                  x += 1
-                end
-              end
-            when AnsiHandler::CsiSequenceType::MoveCursor
-              state.cx = Math.clamp(seq.arg_m.not_nil!.to_i32 - 1, 0, VGA_WIDTH)
-              state.cy = Math.clamp(seq.arg_n.not_nil!.to_i32 - 1, 0, VGA_HEIGHT)
-              move_cursor state.cx, state.cy
-            end
-          when UInt8
-            putchar(state, seq)
-          end
-        end
-      end
-    end
-
-    def print(*args)
-      args.each do |arg|
-        arg.to_s self
-      end
-      VGADevice.lock do |state|
-        move_cursor state.cx, state.cy + 1
-      end
-    end
-
-    private def scroll(state)
-      blank = color_code state.fg, state.bg, ' '.ord.to_u8
-      (VGA_HEIGHT - 1).times do |y|
-        VGA_WIDTH.times do |x|
-          state.buffer[offset x, y] = state.buffer[offset x, (y + 1)]
-        end
-      end
-      VGA_WIDTH.times do |x|
-        state.buffer[VGA_SIZE - VGA_WIDTH + x] = blank
-      end
-      state.wrapback
-    end
-
-    # Cursor
-    def enable_cursor(cursor_start, cursor_end)
-      X86.outb(0x3D4, 0x0A)
-      X86.outb(0x3D5, (X86.inb(0x3D5) & 0xC0) | cursor_start)
-      X86.outb(0x3D4, 0x0B)
-      X86.outb(0x3D5, (X86.inb(0x3D5) & 0xE0) | cursor_end)
-    end
-
-    def disable_cursor
-      X86.outb(0x3D4, 0x0A)
-      X86.outb(0x3D5, 0x20)
-    end
-
-    def move_cursor(x, y)
-      pos = offset x, y
-      X86.outb(0x3D4, 0x0F)
-      X86.outb(0x3D5, (pos & 0xFF).to_u8)
-      X86.outb(0x3D4, 0x0E)
-      X86.outb(0x3D5, ((pos >> 8) & 0xFF).to_u8)
+    lock do |state|
+      state.move_cursor state.cx, state.cy + 1
     end
   end
 end
-
-VGA = VGADevice::Instance.new
