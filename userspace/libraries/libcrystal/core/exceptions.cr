f278258250668cf0452d1d8955e472b4182b8f36
ffwff <mogamiriver@tuta.io>
2020-01-03 14:43:07 +0700

    libcrystal: add __crystal_personality, __crystal_raise, __crystal_get_exception

--- userspace/libraries/libcrystal/core/exceptions.cr
+++ userspace/libraries/libcrystal/core/exceptions.cr
@@ -15,6 +15,17 @@ fun __crystal_raise_overflow : NoReturn
   abort "overflow detected"
 end
 
-macro unimplemented!(file = __FILE__, line = __LINE__)
-  abort "not implemented"
+fun __crystal_personality
+end
+
+fun __crystal_raise(unwind_ex : Void*)
+  abort "__crystal_raise called"
+end
+
+fun __crystal_get_exception(unwind_ex : Void*) : UInt64
+  0u64
+end
+
+macro unimplemented(file = __FILE__, line = __LINE__)
+  abort "{{ file }}:{{ line }}: not implemented"
 end
