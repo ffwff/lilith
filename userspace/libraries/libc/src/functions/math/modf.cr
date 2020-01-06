89c75a545ca35874540eb0f88dbabfee4579ba44
ffwff <mogamiriver@tuta.io>
2020-01-06 12:09:14 +0700

    libc: implement modf, remove fma

--- userspace/libraries/libc/src/functions/math/modf.cr
+++ userspace/libraries/libc/src/functions/math/modf.cr
@@ -1,5 +1,11 @@
-fun modf(arg : Float64, iptr : Float64) : Float64
-  # TODO
-  LibC.abort
-  0.0f64
+fun modf(arg : Float64, iptr : Float64*) : Float64
+  ee = arg.unsafe_as(UInt64).unsafe_shr(52) & 0x7ff
+  if ee == 0x7ff
+    iptr.value = arg
+    return arg
+  end
+  truncated = trunc(arg)
+  decimal = arg - truncated
+  iptr.value = truncated
+  decimal
 end
