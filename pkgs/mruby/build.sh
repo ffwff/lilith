229987fffd780aada979627b17177d8f020e8a6b
ffwff <mogamiriver@tuta.io>
2020-01-06 09:01:42 +0700

    mruby: add cross-build for 64-bit architecture

--- pkgs/mruby/build.sh
+++ pkgs/mruby/build.sh
@@ -4,11 +4,11 @@ build() {
         cd $build_dir/mruby
         try_patch $script_dir/mruby.patch
         [[ $? -gt 1 ]] && exit 1
-        ruby ./minirake -j`nproc` || exit 1
+        CROSSBUILD=lilith64 ruby ./minirake -j`nproc` -v || exit 1
     popd
 }
 
 install() {
-    sudo cp $build_dir/mruby/build/lilith/bin/mruby $install_dir/bin/mruby
-    sudo cp $build_dir/mruby/build/lilith/bin/mirb $install_dir/bin/mirb
+    sudo cp $build_dir/mruby/build/lilith64/bin/mruby $install_dir/bin/mruby
+    sudo cp $build_dir/mruby/build/lilith64/bin/mirb $install_dir/bin/mirb
 }
