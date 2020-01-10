7bf35b860bc172bf297156e4e50b17053e1a3b8d
ffwff <mogamiriver@tuta.io>
2020-01-10 14:26:09 +0700

    libc: implement acos for x86_64

--- userspace/libraries/libc/src/functions/arch/asm/x86_64/math/acos.s
+++ userspace/libraries/libc/src/functions/arch/asm/x86_64/math/acos.s
@@ -1,22 +1,18 @@
-# see ../i386/acos.s
+# use acos(x) = atan2(fabs(sqrt((1-x)*(1+x))), x)
 
 .global acos
 .type acos,@function
 acos:
-  
-  jmp 1f
-
-.global acosl
-.type acosl,@function
-acosl:
-  fldt 8(%rsp)
-1:  fld %st(0)
-  fld1
-  fsub %st(0),%st(1)
-  fadd %st(2)
-  fmulp
-  fsqrt
-  fabs
-  fxch %st(1)
+  movq %xmm0, -8(%rsp)
+  fldl -8(%rsp)
+  mov $0x3ff0000000000000, %rax
+  movq %rax, %xmm2
+  mulsd %xmm0, %xmm0
+  subsd %xmm0, %xmm2
+  sqrtsd %xmm0, %xmm0
+  movq %xmm0, -8(%rsp)
+  fldl -8(%rsp)
   fpatan
+  fstpl -8(%rsp)
+  movq -8(%rsp), %xmm0
   ret
