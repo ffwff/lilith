98d5edc170c7f81db6a53941b3fadf71bfe04136
ffwff <mogamiriver@tuta.io>
2020-01-13 21:07:11 +0700

    tmpfs: use array to store page frames

--- src/fs/impl/tmpfs.cr
+++ src/fs/impl/tmpfs.cr
@@ -1,17 +1,11 @@
 module TmpFS
   extend self
 
-  lib Data
-    
-    # FIXME: this is unportable for non 64-bit architectures
-    MAX_FRAMES = (0x1000 - 8 * 3) // 8
-    struct TmpFSPage
-      prev_page : TmpFSPage*
-      next_page : TmpFSPage*
-      allocated_frames : Int64
-      frames : (UInt8*)[MAX_FRAMES]
+  class FrameArray < Array(UInt8*)
+    def mark(&block : Void* ->)
+      return if @buffer.null?
+      yield @buffer.as(Void*)
     end
-
   end
 
   class Node < VFS::Node
@@ -23,55 +17,8 @@ module TmpFS
     def initialize(@name : String, @fs : FS)
     end
 
-    @first_page = Pointer(Data::TmpFSPage).null
-    @last_page = Pointer(Data::TmpFSPage).null
-    @npages = 0
+    @frames : FrameArray? = nil
     @size = 0
-    
-    private def append_frame
-      if @first_page.null?
-        pframe = FrameAllocator.claim_with_addr | Paging::IDENTITY_MASK
-        @first_page = @last_page = Pointer(Data::TmpFSPage).new pframe
-        zero_page @first_page.as(UInt8*)
-      elsif @last_page.value.allocated_frames == Data::MAX_FRAMES
-        frame = FrameAllocator.claim_with_addr | Paging::IDENTITY_MASK
-        page = Pointer(Data::TmpFSPage).new frame
-        zero_page page.as(UInt8*)
-        page.value.prev_page = @last_page
-        @last_page.value.next_page = page
-        @last_page = page
-      end
-
-      frame = FrameAllocator.claim_with_addr | Paging::IDENTITY_MASK
-      memset Pointer(UInt8).new(frame), 0, 0x1000
-      len = @last_page.value.allocated_frames
-      @last_page.value.frames[len] = Pointer(UInt8).new frame
-      @last_page.value.allocated_frames = len + 1
-    end
-
-    private def pop_frame
-      nlen = @last_page.value.allocated_frames - 1
-      @last_page.value.allocated_frames = nlen
-      frame = @last_page.value.frames[nlen]
-      FrameAllocator.declaim_addr(frame.address & ~Paging::IDENTITY_MASK)
-      @last_page.value.frames[nlen] = Pointer(UInt8).null
-
-      if @last_page.value.allocated_frames == 0
-        prev = @last_page.value.prev_page
-        FrameAllocator.declaim_addr(@last_page.address & ~Paging::IDENTITY_MASK)
-        @last_page = prev
-      end
-    end
-
-    private def each_frame(&block)
-      page = @first_page
-      while !page.null?
-        page.value.allocated_frames.times do |i|
-          yield page.value.frames[i]
-        end
-        page = page.value.next_page
-      end
-    end
 
     def remove : Int32
       return VFS_ERR if removed?
@@ -80,15 +27,11 @@ module TmpFS
         return VFS_ERR
       end
 
-      page = @first_page
-      while !page.null?
-        page.value.allocated_frames.times do |i|
-          frame = page.value.frames[i]
+      if frames = @frames
+        frames.each do |frame|
           FrameAllocator.declaim_addr(frame.address & ~Paging::IDENTITY_MASK)
         end
-        next_page = page.value.next_page
-        FrameAllocator.declaim_addr(page.address & ~Paging::IDENTITY_MASK)
-        page = next_page
+        @frames = nil
       end
 
       @parent.as!(Root).remove_child self
@@ -103,7 +46,7 @@ module TmpFS
 
       foffset = 0
       remaining = Math.min(slice.size, @size)
-      each_frame do |frame|
+      @frames.not_nil!.each do |frame|
         if offset > 0x1000
           offset -= 0x1000
         elsif offset >= 0
@@ -128,7 +71,7 @@ module TmpFS
 
       foffset = 0
       remaining = Math.min(slice.size, @size)
-      each_frame do |frame|
+      @frames.not_nil!.each do |frame|
         if offset > 0x1000
           offset -= 0x1000
         elsif offset >= 0
@@ -143,23 +86,20 @@ module TmpFS
     end
     
     def truncate(size : Int32) : Int32
-      new_npages = size.div_ceil 0x1000
-      if size > @size
+      npages = size.div_ceil 0x1000
+      if @frames.nil?
+        @frames = FrameArray.new(npages)
+      end
+      frames = @frames.not_nil!
+      oldpages = frames.size
+      if npages > oldpages
+        delta = npages - oldpages
+        delta.times do |i|
+          frame = Pointer(UInt8).new(FrameAllocator.claim_with_addr | Paging::IDENTITY_MASK)
+          zero_page frame
+          frames.push(frame)
+        end
         @size = size
-        while @npages < new_npages
-          append_frame
-          @npages += 1
-        end
-      elsif size < @size
-        if @mmap_count > 0
-          Serial.print "tmpfs: can't truncate if mmapd"
-          return @size
-        end
-        @size = size
-        while @npages > new_npages
-          pop_frame
-          @npages -= 1
-        end
       end
       @size
     end
@@ -167,23 +107,28 @@ module TmpFS
     @mmap_count = 0
 
     def mmap(node : MemMapList::Node, process : Multiprocessing::Process) : Int32
-      @mmap_count += 1
-      npages = Math.min(node.size // 0x1000, @npages)
-      i = 0
-      each_frame do |frame|
-        break if i == npages
-        phys = frame.address & ~Paging::IDENTITY_MASK
-        Paging.alloc_page(node.addr + i * 0x1000,
-              node.attr.includes?(MemMapList::Node::Attributes::Write),
-              true, 1, phys,
-              execute: node.attr.includes?(MemMapList::Node::Attributes::Execute))
-        i += 1
+      if frames = @frames
+        @mmap_count += 1
+        npages = Math.min(node.size // 0x1000, frames.size)
+        frames.each_with_index do |frame, idx|
+          break if idx == npages
+          phys = frame.address & ~Paging::IDENTITY_MASK
+          Paging.alloc_page(node.addr + idx * 0x1000,
+                node.attr.includes?(MemMapList::Node::Attributes::Write),
+                true, 1, phys,
+                execute: node.attr.includes?(MemMapList::Node::Attributes::Execute))
+        end
+        VFS_OK
+      else
+        VFS_ERR
       end
-      VFS_OK
     end
 
     def munmap(addr : UInt64, size : UInt64, process : Multiprocessing::Process) : Int32
       @mmap_count -= 1
+      abort "unimplemented"
+      return 0
+      {% if false %}
       i = addr
       end_addr = i + size
       while i < end_addr
@@ -191,6 +136,7 @@ module TmpFS
         i += 0x1000
       end
       VFS_OK
+      {% end %}
     end
   end
 
