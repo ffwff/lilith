ba9dac67877e8b601c3064121a4f847ee7d0f080
aunetx <aunetx@yandex.com>
2020-03-07 16:51:32 +0100

    Changed gcc version and updated gcc.patch

--- userspace/libraries/libc/build.sh
+++ userspace/libraries/libc/build.sh
@@ -3,8 +3,8 @@ build() {
         cd $script_dir && \
         make ARCH=$opt_arch RELEASE=1 -B -j`nproc` libc.a libm.a && \
         make install \
-            LIBDIR=$opt_toolsdir/lib/gcc/$opt_arch/8.3.0/ \
-            INCLUDEDIR=$opt_toolsdir/lib/gcc/$opt_arch/8.3.0/include
+            LIBDIR=$opt_toolsdir/lib/gcc/$opt_arch/10.0.1/ \
+            INCLUDEDIR=$opt_toolsdir/lib/gcc/$opt_arch/10.0.1/include
     popd
 }
 
